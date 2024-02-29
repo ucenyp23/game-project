2cc5a33958d1f1eda8887595ecb4c5de6137b5a8
Pavel Učený <ucenyp23@spstrutnov.cz>
2024-02-29 11:41:30 +0100

    Map

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ width, height = 13, 13
 # Create an empty map
 map = [[' ' for _ in range(width)] for _ in range(height)]
 
-# Add borders
-for i in range(width):
-    map[0][i] = map[height - 1][i] = '#'
-for i in range(height):
-    map[i][0] = map[i][width - 1] = '#'
-
 # Add vertical corridors
 for i in range(height):
-    if random.random() < 0.4 or i % 2 == 0:  # Every other row
+    if random.random() < 0.9 and i % 2 == 0:  # Every other row
         for j in range(width):
-            if random.random() < 0.8 and map[i - 1][j] == ' ':
+            map[i][j] = '#'
+    else:
+        if random.random() < 0.7:
+            for j in range(random.randint(0, int((width - 1)/4))):
+                map[i][j] = '#'
+        if random.random() < 0.7:
+            for j in range(random.randint(int((width - 1)/4)*3, width), width):
                 map[i][j] = '#'
 
 # Add horizontal connections
@@ -24,6 +24,12 @@ for i in range(1, width, 2):  # Every other column, starting from the second
     for _ in range(random.randint(1, 3)):  # 1 to 3 connections
         j = random.randint(0, height - 1)
         map[j][i] = ' '
+
+# Add borders
+for i in range(width):
+    map[0][i] = map[height - 1][i] = '#'
+for i in range(height):
+    map[i][0] = map[i][width - 1] = '#'
 
 # Add enemies
 for _ in range(random.randint(5, 7)):
@@ -34,7 +40,7 @@ for _ in range(random.randint(5, 7)):
             break
 
 # Place the player
-player = next((i, j) for i in range(height - 1, 0, -1) for j in range(width) if map[i][j] == ' ')
+player = next((i, j) for i in range(height - 1, 0, -1) for j in range(width) if map[i][j] == ' ' and map[i][j + 1] == ' ')
 
 map[player[0]][player[1]] = 'P'
 
