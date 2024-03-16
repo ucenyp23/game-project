564317a31903fb0f284ce9f155bef96276849564
ucenyp23 <ucenyp23@spstrutnov.cz>
2024-03-16 07:14:22 +0100

    Forgot Git

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """This module contains a simple game using pygame."""
-# Make this code more funcional and pythonic, clean up it up and make it conform to PEP8.
+from typing import List
 import random
 import pygame
 import heapq
@@ -18,31 +18,34 @@ class Player(pygame.sprite.Sprite):
     """Player sprite class."""
     def __init__(self, position_x, position_y):
         super().__init__()
-        self.image = pygame.Surface((64, 128))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = position_x
-        self.rect.bottom = position_y
+        self.image = self._create_surface((64, 128), GREEN)
+        self.rect = self.image.get_rect(centerx=position_x, bottom=position_y)
         self.vel = pygame.Vector2(0, 0)
         self.jump_counter = 0
         self.speed = 1024
-        self.max_jump = 2
         self.gravity = 16
         self.hp = 1024
-        self.sword_image = pygame.Surface((64, 32))
-        self.sword_image.fill(BLUE)
-        self.sword_rect = self.sword_image.get_rect()
-        self.sword_rect.x = self.rect.centerx
-        self.sword_rect.y = self.rect.centery
+        self.sword_image = self._create_surface((64, 32), BLUE)
+        self.sword_rect = self.sword_image.get_rect(centerx=self.rect.centerx, centery=self.rect.centery)
+        self.direction = 0
 
-    def update(self, delta_time, layout, player):
+    @staticmethod
+    def _create_surface(size, color):
+        surface = pygame.Surface(size)
+        surface.fill(color)
+        return surface
+
+    def update(self, delta_time, layout):
         """Player update function."""
         self.rect.x += self.vel.x * delta_time
+        if self.direction == 1:
+            self.sword_rect.x = self.rect.centerx
+        elif self.direction == -1:
+            self.sword_rect.x = self.rect.centerx - self.sword_image.get_width()
         self._collisions(layout, 'x')
         self.rect.y += self.vel.y * delta_time
+        self.sword_rect.centery = self.rect.centery
         self._collisions(layout, 'y')
-        self.sword_rect.centerx = self.rect.centerx
-        self.sword_rect.centery = self.rect.centery - 3320
 
     def _collisions(self, layout, direction):
         collision = False
@@ -76,10 +79,11 @@ class Player(pygame.sprite.Sprite):
     def move(self, direction):
         """Player move function."""
         self.vel.x = direction * self.speed
+        self.direction = direction
 
     def jump(self):
         """Player jump function."""
-        if self.jump_counter < self.max_jump:
+        if self.jump_counter < 2:
             self.vel.y = -self.speed
             self.jump_counter += 1
 
@@ -92,16 +96,19 @@ class Kamikaze(pygame.sprite.Sprite):
     """Kamikaze sprite class."""
     def __init__(self, position_x, position_y):
         super().__init__()
-        self.image = pygame.Surface((32, 32))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = position_x
-        self.rect.bottom = position_y
+        self.image = self._create_surface((32, 32), RED)
+        self.rect = self.image.get_rect(centerx = position_x, bottom = position_y)
         self.vel = pygame.Vector2(0, 0)
         self.speed = 2048
         self.hp = 64
         self.enable = False
         self.visible = False
+
+    @staticmethod
+    def _create_surface(size, color):
+        surface = pygame.Surface(size)
+        surface.fill(color)
+        return surface
 
     def move(self, player, layout):
         """Move towards the player."""
@@ -111,32 +118,21 @@ class Kamikaze(pygame.sprite.Sprite):
                     tile_rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
                     if not tile_rect.clipline((self.rect.centerx, self.rect.centery), (player.rect.centerx, player.rect.centery)):
                         self.visible = True
+                        player_x = player.rect.centerx - self.rect.centerx
+                        player_y = player.rect.centerx - self.rect.centerx
                     else:
                         self.visible = False
-        
-        if self.visible == True and \
-            -500 < (player.rect.centerx - self.rect.centerx) < 500 and \
-            -500 < (player.rect.centery - self.rect.centery) < 500:
-            direction = pygame.Vector2((player.rect.centerx - self.rect.centerx) / 500, (player.rect.centery - self.rect.centery) / 500)
+
+        if self.visible:
+            direction = pygame.Vector2(player_x / (SCREEN_WIDTH // 2), player_y / (SCREEN_HEIGHT // 2))
             self.vel = direction * self.speed
-
-    def enabling(self, player):
-        """Enable movement."""
-        direction = pygame.Vector2(player.rect.x - self.rect.x, (player.rect.y + player.rect.height // 2) - self.rect.y)
-        within_screen = -TILE_SIZE*3 < direction.x < TILE_SIZE*3 and -TILE_SIZE*0.9 < direction.y < TILE_SIZE*0.9
-
-        if within_screen:
-            self.enable = True
 
     def update(self, delta_time, layout, player):
         """Kamikaze update function."""
         if player.rect.colliderect(self.rect):
             player.hp -= self.hp
             self.hp = 0
-        if self.enable == False:
-            self.enabling(player)
-        else:
-            self.move(player, layout)
+        self.move(player, layout)
         self.rect.x += self.vel.x * delta_time
         self._collisions(layout, 'x')
         self.rect.y += self.vel.y * delta_time
@@ -316,15 +312,16 @@ class Scarecrow(pygame.sprite.Sprite):
         """Scarecrow draw function."""
         screen.blit(self.image, self.rect)
 
-def generate_map():
+def generate_map(map_size: int) -> List[List[str]]:
     """Map generation function"""
-    size_5 = MAP_SIZE // 5
-    size_1 = MAP_SIZE - 1
-    avoid_chars = ['P', '1', '2', '3']
+    size_1 = map_size - 1
+    entities = ['P', '1', '2', '3']
 
-    def generate():
+    def generate_layout(map_size: int, size_1: int) -> List[List[str]]:
+        size_5 = map_size // 5
         layout = [['#' if i % 2 == 0 or j in {0, size_1} or i in {0, size_1} else ' '
-                   for j in range(MAP_SIZE)] for i in range(MAP_SIZE)]
+                   for j in range(map_size)] for i in range(map_size)]
+
         for i in range(1, size_1):
             if i % 2 == 0:
                 rand_range = random.randrange(1, size_5)
@@ -334,19 +331,18 @@ def generate_map():
                         layout[i][clime] = ' '
             else:
                 ml, mr = sorted(random.randrange(size_5 * 2, size_5 * 3) for _ in range(2))
-                rand_range_start = random.randrange(1, size_5)
-                rand_range_end = random.randrange(size_5 * 4, MAP_SIZE)
-                for j in list(range(rand_range_start)) + \
-                        list(range(rand_range_end, size_1)) + \
+                for j in list(range(random.randrange(1, size_5))) + \
+                        list(range(random.randrange(size_5 * 4, map_size), size_1)) + \
                         list(range(ml, mr)):
                     if layout[i - 1][j] == '#':
                         layout[i][j] = '#'
+
         return layout
 
-    def validate(layout):
+    def validate_layout(layout: List[List[str]], map_size: int) -> bool:
         visited = set()
-        start = next((i, j) for i in range(MAP_SIZE)
-                    for j in range(MAP_SIZE) if layout[i][j] == ' ')
+        start = next((i, j) for i in range(map_size)
+                    for j in range(map_size) if layout[i][j] == ' ')
 
         stack = [start]
         while stack:
@@ -355,30 +351,99 @@ def generate_map():
                 visited.add((i, j))
                 for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                     ni, nj = i + di, j + dj
-                    if 0 <= ni < MAP_SIZE and 0 <= nj < MAP_SIZE and layout[ni][nj] == ' ':
+                    if 0 <= ni < map_size and 0 <= nj < map_size and layout[ni][nj] == ' ':
                         stack.append((ni, nj))
 
         return all(layout[i][j] != ' ' or (i, j) in visited
-                    for i in range(MAP_SIZE) for j in range(MAP_SIZE))
+                    for i in range(map_size) for j in range(map_size))
 
-    layout = generate()
-    while not validate(layout):
-        layout = generate()
+    layout = generate_layout(map_size, size_1)
+    while not validate_layout(layout, map_size):
+        layout = generate_layout(map_size, size_1)
 
-    player = next((i, j) for i in range(size_1, 0, -1)
-                    for j in range(MAP_SIZE) if layout[i][j] == ' ' and layout[i][j + 1] == ' ')
-    layout[player[0]][player[1]] = 'P'
+    player = next(
+        (i, j)
+        for i in range(size_1 - 1, 1, -2)
+        for j in range(1, size_1)
+        if layout[i][j] == ' '
+    )
+    layout[player[0]][player[1]] = entities[0]
 
-    for _ in range(random.randrange(5, 8)):
+    for _ in range(random.randrange(5, 13)):
         while True:
-            i, j = random.randrange(1, MAP_SIZE - 2, 2), random.randrange(1, MAP_SIZE - 1, 2)
-            if (layout[i][j] == ' ' and layout[i + 1][j] == '#' and
-                layout[i][j - 1] not in avoid_chars and
-                layout[i][j + 1] not in avoid_chars):
-                layout[i][j] = random.choice(['1', '2', '3'])
+            i, j = random.randrange(1, map_size - 1, 2), random.randrange(1, size_1)
+            if layout[i][j] == ' ' and layout[i + 1][j] == '#':
+                layout[i][j] = random.choice(entities[1:])
                 break
 
     return layout
+
+def main_menu(screen):
+    """Main menu function."""
+    font = pygame.font.Font(None, 128)
+    play_text = font.render('Play', True, WHITE)
+    quit_text = font.render('Quit', True, WHITE)
+    play_rect = pygame.Rect(SCREEN_WIDTH // 2 - play_text.get_width() // 2,
+    SCREEN_HEIGHT // 2 - play_text.get_height(), play_text.get_width(), play_text.get_height())
+    quit_rect = pygame.Rect(SCREEN_WIDTH // 2 - quit_text.get_width() // 2,
+    SCREEN_HEIGHT // 2 + quit_text.get_height(), quit_text.get_width(), quit_text.get_height())
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.MOUSEBUTTONUP:
+                if play_rect.collidepoint(pygame.mouse.get_pos()):
+                    return True
+                if quit_rect.collidepoint(pygame.mouse.get_pos()):
+                    return False
+
+        screen.fill(BLACK)
+        screen.blit(play_text, play_rect)
+        screen.blit(quit_text, quit_rect)
+        pygame.display.update()
+
+def level(screen):
+    """Level function."""
+    clock = pygame.time.Clock()
+    layout = generate_map(MAP_SIZE)
+    player = create_player(layout)
+    enemies = create_enemies(layout)
+    while handle_events(player):
+        delta_time = clock.get_time() / 1000
+        enemies.update(delta_time, layout, player)
+        player.update(delta_time, layout)
+        camera_x, camera_y = update_camera(player, layout, screen)
+        draw_tiles(layout, screen, camera_x, camera_y)
+        update_positions(enemies, camera_x, camera_y, player)
+        enemies = pygame.sprite.Group(enemy for enemy in enemies if enemy.hp != 0)
+        for enemy in enemies:
+            enemy.draw(screen)
+        player.draw(screen)
+        reset_positions(enemies, camera_x, camera_y, player)
+        pygame.display.update()
+        clock.tick(60)
+
+def create_player(layout):
+    """Player creation function."""
+    for y, row in enumerate(layout):
+        for x, tile in enumerate(row):
+            if tile == 'P':
+                return Player(x*TILE_SIZE + TILE_SIZE // 2, (y + 1)*TILE_SIZE)
+
+
+def create_enemies(layout):
+    """Enemies creation function."""
+    entities = {'1': Kamikaze, '2': Slasher, '3': Lancer}
+    enemies = pygame.sprite.Group()
+
+    for y, row in enumerate(layout):
+        for x, tile in enumerate(row):
+            if tile in entities:
+                entity = entities[tile](x*TILE_SIZE + TILE_SIZE // 2, y*TILE_SIZE if tile == '1' else (y + 1)*TILE_SIZE)
+                enemies.add(entity)
+
+    return enemies
 
 def handle_events(player):
     """Events handling function."""
@@ -398,71 +463,13 @@ def handle_events(player):
 
     return True
 
-def main_menu(screen):
-    """Main menu function."""
-    menu_font = pygame.font.Font(None, 128)
-    play_text = menu_font.render('Play', True, WHITE)
-    quit_text = menu_font.render('Quit', True, WHITE)
-    play_rect = pygame.Rect(SCREEN_WIDTH // 2 - play_text.get_width() // 2,
-    SCREEN_HEIGHT // 2 - play_text.get_height(), play_text.get_width(), play_text.get_height())
-    quit_rect = pygame.Rect(SCREEN_WIDTH // 2 - quit_text.get_width() // 2,
-    SCREEN_HEIGHT // 2 + quit_text.get_height(), quit_text.get_width(), quit_text.get_height())
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-            if event.type == pygame.MOUSEBUTTONUP:
-                if play_rect.collidepoint(pygame.mouse.get_pos()):
-                    return True
-                if quit_rect.collidepoint(pygame.mouse.get_pos()):
-                    return False
-
-        screen.fill(BLACK)
-        screen.blit(play_text, play_rect.topleft)
-        screen.blit(quit_text, quit_rect.topleft)
-        pygame.display.update()
-
-def level(screen):
-    """Level function."""
-    clock = pygame.time.Clock()
-    sprites = pygame.sprite.Group()
-    layout = generate_map()
-    tile_to_class = {'P': Player, '1': Kamikaze, '2': Slasher, '3': Lancer}
-    player = create_entities(layout, sprites, tile_to_class)
-    while handle_events(player):
-        sprites.update(clock.get_time() / 1000, layout, player)
-        camera_x, camera_y = update_camera(player, layout, screen)
-        draw_tiles(layout, screen, camera_x, camera_y)
-        update_positions(sprites, camera_x, camera_y, player)
-        for sprite in sprites:
-            if sprite.hp == 0:
-                sprites.remove(sprite)
-        sprites.draw(screen)
-        print("Player: " + str(player.rect))
-        print("Sword: " + str(player.sword_rect))
-        reset_positions(sprites, camera_x, camera_y, player)
-        pygame.display.update()
-        clock.tick(60)
-
-def create_entities(layout, sprites, tile_to_class):
-    """Entity creation function."""
-    for y, row in enumerate(layout):
-        for x, tile in enumerate(row):
-            if tile in tile_to_class:
-                entity = tile_to_class[tile](x*TILE_SIZE + TILE_SIZE // 2,
-                                            y*TILE_SIZE if tile == '1' else (y + 1)*TILE_SIZE)
-                sprites.add(entity)
-                if tile == 'P':
-                    player = entity
-    return player
-
 def update_camera(player, layout, screen):
     """Camera update function."""
     camera_x = min(max(player.rect.centerx - screen.get_width() // 2, 0),
                     len(layout[0])*TILE_SIZE - screen.get_width())
     camera_y = min(max(player.rect.centery - screen.get_height() // 2, 0),
                     len(layout)*TILE_SIZE - screen.get_height())
+
     return camera_x, camera_y
 
 def draw_tiles(layout, screen, camera_x, camera_y):
@@ -477,23 +484,25 @@ def draw_tiles(layout, screen, camera_x, camera_y):
                                         TILE_SIZE, TILE_SIZE)
                     pygame.draw.rect(screen, WHITE, tile)
 
-def update_positions(sprites, camera_x, camera_y, player):
+def update_positions(enemies, camera_x, camera_y, player):
     """Update position function."""
     player.rect.centerx -= camera_x
     player.rect.centery -= camera_y
-    for sprite in sprites:
-        if sprite != player:
-            sprite.rect.centerx -= camera_x
-            sprite.rect.centery -= camera_y
+    player.sword_rect.centerx -= camera_x
+    player.sword_rect.centery -= camera_y
+    for enemy in enemies:
+        enemy.rect.centerx -= camera_x
+        enemy.rect.centery -= camera_y
 
-def reset_positions(sprites, camera_x, camera_y, player):
+def reset_positions(enemies, camera_x, camera_y, player):
     """Reset position function."""
     player.rect.centerx += camera_x
     player.rect.centery += camera_y
-    for sprite in sprites:
-        if sprite != player:
-            sprite.rect.centerx += camera_x
-            sprite.rect.centery += camera_y
+    player.sword_rect.centerx += camera_x
+    player.sword_rect.centery += camera_y
+    for enemy in enemies:
+        enemy.rect.centerx += camera_x
+        enemy.rect.centery += camera_y
 
 def main():
     """Main function."""
